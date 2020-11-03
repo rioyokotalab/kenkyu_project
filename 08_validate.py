caed1e001543f00187c79c410de89385fa36abe9
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-11-03 22:52:52 +0900

    Use train() and main()

--- 08_validate.py
+++ 08_validate.py
@@ -30,18 +30,17 @@ train_dataset = datasets.MNIST('./data',
                                download=True, 
                                transform=transforms.ToTensor())
 
-validation_dataset = datasets.MNIST('./data', 
-                                    train=False, 
-                                    transform=transforms.ToTensor())
+val_dataset = datasets.MNIST('./data', 
+                             train=False, 
+                             transform=transforms.ToTensor())
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
                                            batch_size=batch_size, 
                                            shuffle=True)
 
-validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, 
-                                                batch_size=batch_size, 
-                                                shuffle=False)
-
+val_loader = torch.utils.data.DataLoader(dataset=val_dataset, 
+                                         batch_size=batch_size, 
+                                         shuffle=False)
 
 # define model
 model = TwoLayerNet(D_in, H, D_out)
@@ -52,27 +51,20 @@ criterion = nn.CrossEntropyLoss()
 # define optimizer
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-def validate(loss_vector, accuracy_vector):
+def validate():
     model.eval()
     val_loss, correct = 0, 0
-    for data, target in validation_loader:
+    for data, target in val_loader:
         output = model(data)
         val_loss += criterion(output, target).data.item()
         pred = output.data.max(1)[1] # get the index of the max log-probability
         correct += pred.eq(target.data).cpu().sum()
 
-    val_loss /= len(validation_loader)
-    loss_vector.append(val_loss)
-
-    accuracy = 100. * correct.to(torch.float32) / len(validation_loader.dataset)
-    accuracy_vector.append(accuracy)
-    
+    val_loss /= len(val_loader)
+    val_acc = 100. * correct.to(torch.float32) / len(val_loader.dataset)
     print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        val_loss, correct, len(validation_loader.dataset), accuracy))
+        val_loss, correct, len(val_loader.dataset), val_acc))
 
-
-
-lossv, accv = [], []
 for epoch in range(epochs):
     # Set model to training mode
     model.train()
@@ -100,4 +92,4 @@ for epoch in range(epochs):
                 time.perf_counter() - t))
             t = time.perf_counter()
 
-    validate(lossv, accv)
+    validate()
---
+++ 13_ddp.py
@@ -0,0 +1,118 @@
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.nn.parallel import DistributedDataParallel as DDP
+import time
+import os
+
+def print0(message):
+    if torch.distributed.is_initialized():
+        if torch.distributed.get_rank() == 0:
+            print(message, flush=True)
+    else:
+        print(message, flush=True)
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        output = F.log_softmax(x, dim=1)
+        return output
+
+def train(train_loader,model,criterion,optimizer,epoch,device):
+    model.train()
+    t = time.perf_counter()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data = data.to(device)
+        target = target.to(device)
+        output = model(data)
+        loss = criterion(output, target)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 200 == 0:
+            print0('Train Epoch: {} [{:>5}/{} ({:.0%})]\tLoss: {:.6f}\t Time:{:.4f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                batch_idx / len(train_loader), loss.data.item(),
+                time.perf_counter() - t))
+            t = time.perf_counter()
+
+def validate(val_loader,model,criterion,device):
+    model.eval()
+    val_loss, correct = 0, 0
+    for data, target in val_loader:
+        data = data.to(device)
+        target = target.to(device)
+        output = model(data)
+        val_loss += criterion(output, target).data.item()
+        pred = output.data.max(1)[1]
+        correct += pred.eq(target.data).cpu().sum()
+
+    val_loss /= len(val_loader)
+    val_acc = 100. * correct.to(torch.float32) / len(val_loader.dataset)
+    print0('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(val_loader.dataset), val_acc))
+
+def main():
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '8888'
+    rank = int(os.getenv('OMPI_COMM_WORLD_RANK', '0'))
+    world_size = int(os.getenv('OMPI_COMM_WORLD_SIZE', '1'))
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+    device = torch.device('cuda',rank)
+
+    epochs = 10
+    batch_size = 32
+    learning_rate = 1.0e-02 * world_size
+
+    train_dataset = datasets.MNIST('./data',
+                                   train=True,
+                                   download=True,
+                                   transform=transforms.ToTensor())
+    val_dataset = datasets.MNIST('./data',
+                                 train=False,
+                                 transform=transforms.ToTensor())
+    train_sampler = torch.utils.data.distributed.DistributedSampler(
+        train_dataset,
+        num_replicas=torch.distributed.get_world_size(),
+        rank=torch.distributed.get_rank())
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               sampler=train_sampler)
+    val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=False)
+    model = CNN().to(device)
+    model = DDP(model, device_ids=[rank])
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    for epoch in range(epochs):
+        model.train()
+        train(train_loader,model,criterion,optimizer,epoch,device)
+        validate(val_loader,model,criterion,device)
+
+    dist.destroy_process_group()
+
+if __name__ == '__main__':
+    main()
