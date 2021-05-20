9b6d35d43760779c72ccbac7087f906c9536c49d
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-05-20 17:19:54 +0900

    Update wrapper

--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,6 @@
 import os
 import sys
 args = ' '.join(map(str,sys.argv[1:]))
-command = f'mpirun -npernode 4 -np 4 python 19_regularization.py {args}'
+command = f'mpirun -np 4 python 19_regularization.py {args}'
 print(command)
 os.system(command)
